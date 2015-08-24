59469d499e895302f93f2109a00ac3c7ba67751e
NaokiStones <maxwell.fse@gmail.com>
2015-08-24 12:37:02 +0900

    t

---
+++ src/main/java/hivemall/lda/OnlineLDA.java
@@ -0,0 +1,20 @@
+package hivemall.lda;
+
+import hivemall.io.Feature;
+import hivemall.io.LDAModel;
+
+public class OnlineLDA implements LDAModel{
+	
+	public OnlineLDA(int K, int D, double alpha, double eta, double tau, double dappa){
+	}
+	
+	public void workOn(Feature[] x, int time){
+		// Initialize rhot
+		
+		// E Step
+		
+		
+		
+	}
+
+}
